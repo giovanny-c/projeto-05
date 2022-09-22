212acf9769547ef778d287aadfc9fa7e65677436
Giovanny Castanharo <giovanycast@hotmail.com>
2022-09-22 16:10:02 -0300

    handling client input errors with celebrate

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,14 +1,60 @@
 import { Router } from "express"
 import ProductsController from "../controllers/ProductsController"
-
+import { celebrate, Joi, Segments } from "celebrate" // @types/joi tbm
+import { join } from "path"
 const productRoutes = Router()
 
 const productsController = new ProductsController()
 
 productRoutes.get("/", productsController.index)
-productRoutes.post("/create", productsController.create)
-productRoutes.get("/:id", productsController.show)
-productRoutes.put("/:id/update", productsController.update)
-productRoutes.delete("/:id/delete", productsController.delete)
+
+productRoutes.post(
+    "/create",
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required(),
+        },
+    }),
+    productsController.create,
+)
+
+productRoutes.get(
+    "/:id",
+    celebrate({
+        //middleware
+        [Segments.PARAMS]: {
+            // do req.params
+            id: Joi.string().uuid().required(), // id do tipo string(uuid), obrigatorio
+        },
+    }),
+    productsController.show,
+)
+
+productRoutes.put(
+    "/:id/update",
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required(),
+        },
+    }),
+    productsController.update,
+)
+
+productRoutes.delete(
+    "/:id/delete",
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    productsController.delete,
+)
 
 export default productRoutes
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,8 @@
-import express, { NextFunction, Request, Response } from "express"
+import express from "express"
 import cors from "cors"
-
 import routes from "./routes"
-import AppError from "shared/erros/AppError"
+import { errors as celebrateErrors } from "celebrate"
+import { errorHandler } from "shared/erros/errorHandler"
 
 const app = express()
 
@@ -11,18 +11,8 @@ app.use(express.json())
 
 app.use(routes)
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-        return res.status(error.statusCode).json({
-            status: "error",
-            message: error.message,
-        })
-    }
+app.use(celebrateErrors())
 
-    return res.status(500).json({
-        status: "error",
-        message: "Internal server error.",
-    })
-})
+app.use(errorHandler)
 
 export { app }
